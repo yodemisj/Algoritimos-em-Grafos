899aa862d93fa8e2d147b8d6c393332fa1908859
yodemisj <yodemisj@gmail.com>
2024-08-05 10:20:12 -0300

    versão experimental

--- puzzle/graph.h
+++ puzzle/graph.h
@@ -1,12 +1,170 @@
 #pragma once
 #include <iostream>
 #include <vector>
+#include <map>
+#include <queue>
+#include <cmath>
+
 using namespace std;
+
+typedef pair<vector<int>, int> Board;
+// typedef vector<int> Board;
 
 class Graph {
 private: 
-    vector<vector<int>> adj;
+    map<Board,vector<Board>> adj;
+    vector<vector<int>> m;
 
 public:
+
+Graph(Board& board, int puzzleSize, int linesize) {
+    adj[board] = vector<Board>();
+    initM(m, 9, 3);
+}
+
+void initM(vector<vector<int>>& m, int puzzleSize, int lineSize) {
+    m = vector<vector<int>>(puzzleSize, vector<int>(puzzleSize,0));
+    for(int i = 1; i < puzzleSize; i++) {
+        for(int j = 0; j < puzzleSize; j++) {
+            m[i][j] = calculateMovesToPosition(i, j, lineSize);
+        }
+        cout<<endl;
+    }
+}
+
+int searchEmptySpace (const vector<int>& puzzle) {
+    for(int i = 0; i < puzzle.size(); i++) {
+        if(puzzle[i] == 0) return i;
+    }
+
+    return -1;
+}
+
+int getRowByIndex(const int index, const int lineSize) {
+    return ceil((index + 1.0) / lineSize);
+}
+
+int getlineStartIndex(const int index, const int lineSize) {
+    return (getRowByIndex(index, lineSize) - 1)  * lineSize;
+}
+
+int getColumnByIndex(const int index, const int lineSize) {
+    return index - getlineStartIndex(index, lineSize);
+}
+
+int calculateMovesToPosition(const int position, const int destination, const int lineSize) {
+    int currentLine = getRowByIndex(position, lineSize);
+    int destinationLine = getRowByIndex(destination, lineSize);
+    int verticalDistance = abs(currentLine - destinationLine);
+    int currentColumn = getColumnByIndex(position, lineSize);
+    int destinationColumn = getColumnByIndex(destination, lineSize);
+    int horizontalDistance = abs(currentColumn - destinationColumn);
+
+    return verticalDistance + horizontalDistance;
     
+}
+
+int heuristic(const vector<int>& board) {
+    int total = 0;
+
+    for(int i = 0; i < board.size(); i++) {  
+        if(board[i] == 0) continue;
+        total += m[board[i]][i];
+    }
+    return total;
+}
+
+void addNeighbors(const Board& board, const int lineSize) {
+    int whiteSpaceIndex = searchEmptySpace(board.first);
+    if(whiteSpaceIndex < 0) return;
+
+    int lineStartIndex = getlineStartIndex(whiteSpaceIndex, lineSize);
+    int lineEndIndex = getRowByIndex(whiteSpaceIndex, lineSize) * lineSize - 1;
+    Board copy;
+
+    // Up
+    int currentIndex = whiteSpaceIndex - lineSize;
+    if(!(currentIndex < 0)) {
+        copy = board;
+        copy.second = INT32_MAX;
+        swap(copy.first[currentIndex], copy.first[whiteSpaceIndex]);
+        if(adj.count(copy) == 0) adj[copy] = vector<Board>();
+        adj[board].push_back(copy);
+    }
+    // Down
+    currentIndex = whiteSpaceIndex + lineSize;
+    if(!(currentIndex > (board.first.size() - 1))){
+        copy = board;
+        copy.second = INT32_MAX;
+        swap(copy.first[currentIndex], copy.first[whiteSpaceIndex]);
+        if(adj.count(copy) == 0) adj[copy] = vector<Board>();
+        adj[board].push_back(copy);
+    } 
+
+    // Left
+    currentIndex = whiteSpaceIndex - 1;
+    if(!(currentIndex < lineStartIndex)) {
+        copy = board;
+        copy.second = INT32_MAX;
+        swap(copy.first[currentIndex], copy.first[whiteSpaceIndex]);
+        if(adj.count(copy) == 0) adj[copy] = vector<Board>();
+        adj[board].push_back(copy);
+    } 
+
+    // Right
+    currentIndex = whiteSpaceIndex + 1;
+    if(!(currentIndex > lineEndIndex)) {
+        copy = board;
+        copy.second = INT32_MAX;
+        swap(copy.first[currentIndex], copy.first[whiteSpaceIndex]);
+        if(adj.count(copy) == 0) adj[copy] = vector<Board>();
+        adj[board].push_back(copy);
+    } 
+}
+
+void a_star(Board start, Board goal, int lineSize) {
+    auto compare = [](const Board& a, const Board& b) {
+        return a.second > b.second;
+    };
+    map<Board, int> dist;
+    map<Board, Board> pi;
+    vector<Board> S;
+    priority_queue<Board, vector<Board>, decltype(compare)> Q(compare);
+    dist[start] = 0;
+    start.second = 0;
+    Q.push(start);
+
+    // int i = 0;
+    while(!Q.empty()) {
+        Board current = Q.top();
+        Q.pop();
+        S.push_back(current);
+        // cout<<i++<<endl;
+
+        for(auto number : current.first) {
+            cout << number << " ";
+        }
+        cout<<endl;
+        if(current.first == goal.first) {
+            cout<<"achou"<<endl;
+            
+            break;
+        }
+
+        addNeighbors(current,lineSize);
+
+        for(Board& neighbor : adj[current]) {
+            if(dist.count(neighbor) == 0) dist[neighbor] = INT32_MAX;
+
+            if(dist[neighbor] > dist[current] + 1){
+                dist[neighbor] = dist[current] + 1;
+                neighbor.second = dist[neighbor] + heuristic(neighbor.first);
+                pi[neighbor] = current;
+                Q.push(neighbor);
+            }
+        }
+    }
+}   
+
+
 };
--- puzzle/main.cpp
+++ puzzle/main.cpp
@@ -1,86 +1,18 @@
 #include <iostream>
-#include <vector>
-#include <cmath>
 #include "graph.h"
 
 using namespace std;
 
-int searchEmptySpace (const vector<int>& puzzle) {
-    for(int i = 0; i < puzzle.size(); i++) {
-        if(puzzle[i] == 0) return i;
-    }
-
-    return -1;
-}
-
-int getRowByIndex(const int index, const int lineSize) {
-    return ceil((index + 1.0) / lineSize);
-}
-
-int getlineStartIndex(const int index, const int lineSize) {
-    return (getRowByIndex(index, lineSize) - 1)  * lineSize;
-}
-
-int getColumnByIndex(const int index, const int lineSize) {
-    return index - getlineStartIndex(index, lineSize);
-}
-
-void findNeighbors(const vector<int>& puzzle, const int lineSize, vector<int>& neighbors) {
-    int whiteSpaceIndex = searchEmptySpace(puzzle);
-    if(whiteSpaceIndex < 0) return;
-
-    int lineStartIndex = getlineStartIndex(whiteSpaceIndex, lineSize);
-    int lineEndIndex = getRowByIndex(whiteSpaceIndex, lineSize) * lineSize - 1;
-
-    // Up
-    int currentIndex = whiteSpaceIndex - lineSize;
-    if(!(currentIndex < 0)) neighbors.push_back(currentIndex);
-
-    // Down
-    currentIndex = whiteSpaceIndex + lineSize;
-    if(!(currentIndex > (puzzle.size() - 1))) neighbors.push_back(currentIndex);
-
-    // Left
-    currentIndex = whiteSpaceIndex - 1;
-    if(!(currentIndex < lineStartIndex)) neighbors.push_back(currentIndex);
-
-    // Right
-    currentIndex = whiteSpaceIndex + 1;
-    if(!(currentIndex > lineEndIndex)) neighbors.push_back(currentIndex);
-
-}
-
-int calculateMovesToPosition(const int position, const int destination, const int lineSize) {
-    int currentLine = getRowByIndex(position, lineSize);
-    int destinationLine = getRowByIndex(destination, lineSize);
-    int verticalDistance = abs(currentLine - destinationLine);
-    int currentColumn = getColumnByIndex(position, lineSize);
-    int destinationColumn = getColumnByIndex(destination, lineSize);
-    int horizontalDistance = abs(currentColumn - destinationColumn);
-
-    return verticalDistance + horizontalDistance;
-    
-}
-
-int calculateTotal(const vector<int>& puzzle, const int lineSize) {
-    int total = 0;
-
-    for(int i = 0; i < puzzle.size(); i++) {  
-        if(puzzle[i] == 0) continue;
-        total += calculateMovesToPosition(i, puzzle[i], lineSize);
-    }
-    return total;
-}
-
 int main() {
-    vector<int> puzzle = {5, 6, 2, 7, 1, 8, 3, 4, 0};
-    vector<int> neighbors;
-    // 5 6 2
-    // 7 1 8
-    // 3 4 0
+    vector<vector<int>> m(9, vector<int>(9,0));
+    vector<int> board = {5, 6, 2, 7, 1, 8, 3, 4, 0};
+    vector<int> goal = {0, 1, 2, 3, 4, 5, 6, 7, 8};
+    vector<int> start = {1, 0, 2, 3, 4, 5, 6, 7, 8};
+    Board b1 = {board,INT32_MAX};
+    Board b2 = {goal,INT32_MAX};
+    Graph graph(b1, 9, 3);
 
-
-    cout << calculateTotal(puzzle, 3) << endl;
+    graph.a_star(b1, b2,3);
 
     return 0;
 }
