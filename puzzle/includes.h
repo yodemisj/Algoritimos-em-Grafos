34d58ec3aa38accc86ac568e4b8319e03e39988c
yodemisj <yodemisj@gmail.com>
2024-08-29 10:25:24 -0300

    refact: move includes to this file

---
+++ puzzle/includes.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include <algorithm>
+#include <sstream>
+#include <map>
+#include <set>
+#include <queue>
+#include <cmath>
+#include <chrono>
+
+using namespace std;
+
+typedef pair<vector<int>, int> Board;
