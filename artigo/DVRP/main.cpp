bdaa1d516617e74e5250872ace62482dc1e93cc0
yodemisj <yodemisj@gmail.com>
2024-09-16 11:28:42 -0300

    wip: genetic algorithm

--- artigo/DVRP/main.cpp
+++ artigo/DVRP/main.cpp
@@ -1,67 +1,12 @@
-// #include <iostream>
-// #include <random>
-// #include <vector>
-// #include <algorithm>
-// #include <limits>
 #include "Graph.h"
 
 using namespace std; 
 
-// const int INF = numeric_limits<int>::infinity();
-
 void printVector(vector<int> v) {
-    for(int i = 0; i < v.size(); i++) {
+    for(size_t i = 0; i < v.size(); i++) {
         cout << v[i] << " ";
     }
     cout << "\n";
-}
-
-void crossoverOX(const vector<int> p1, const vector<int> p2, vector<int>& c1, vector<int>& c2) {
-    int p1Size = p1.size();
-    int p2Size = p2.size();
-
-    if(p1Size != p2Size) {
-        return; 
-    }
-
-    random_device rd;
-    mt19937 gen(rd());
-    uniform_int_distribution<> dis(1, p1Size - 2);
-    int i = dis(gen);
-    int j = dis(gen);
-
-    while (j <= i) {
-        j = dis(gen);
-    }
-
-    cout << "i: " << i << " j: " << j << "\n";
-
-    for(int k = i; k <= j; k++) {
-        c1[k] = p1[k];
-        c2[k] = p2[k];
-    }
-    
-    int indexC1 = (j + 1) % p1Size; 
-    int indexC2 = (j + 1) % p1Size; 
-
-    for (int k = j + 1; k < p1Size + j + 1; k++) {
-        int index = k % p1Size;
-
-        auto itP1 = find(c2.begin(), c2.end(), p1[index]);
-        auto itP2 = find(c1.begin(), c1.end(), p2[index]);
-
-        if(itP1 == c2.end()) {
-            c2[indexC2] = p1[index];
-            indexC2++;
-            indexC2 %= p1Size;
-        }
-
-        if(itP2 == c1.end()) {
-            c1[indexC1] = p2[index];
-            indexC1++;
-            indexC1 %= p1Size;
-        }
-    }
 }
 
 void initializeGraph(Graph& graph) {
@@ -92,14 +37,23 @@ void initializeGraph(Graph& graph) {
     vector<int> V(6);
     vector<int> P(6);
     // vector<int> trip(6);
-    vector<int> trip;
+    vector<vector<int>> trip;
 
-    graph.splitProcedure(5, V, P, 10, INF);
-    graph.extractSolution(5, P, trip);
+    graph.splitProcedure(P, 10, INF);
+    graph.extractSolution(P, trip);
+    // trip = graph.extractSolution(P, 5);
 
-    printVector(V);
+    // printVector(V);
     printVector(P);
-    printVector(trip);
+    // printVector(trip);
+
+    for (size_t i = 0; i < trip.size(); ++i) {
+        cout << "Rota " << i + 1 << ": ";
+        for (int customer : trip[i]) {
+            cout << customer << " ";
+        }
+        cout << endl;
+    }
 }
 
 int main() {
